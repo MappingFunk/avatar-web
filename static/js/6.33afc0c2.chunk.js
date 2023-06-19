b6be5172fb53c005209b1a0961103ecf65f2f80f
lizhong <LZ0203162@yeah.net>
2023-06-19 18:01:10 +0800

    init

---
+++ static/js/6.33afc0c2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[6],{985:function(n,c){},994:function(n,c){},996:function(n,c){}}]);
+//# sourceMappingURL=6.33afc0c2.chunk.js.map
---
+++ static/js/8.16cd7cd7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[8],{838:function(n,p){}}]);
+//# sourceMappingURL=8.16cd7cd7.chunk.js.map
